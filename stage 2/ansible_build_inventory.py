342e1bbcaac0cbf6492187f973263042302e548d
Antikuz <36120950+Antikuz@users.noreply.github.com>
2021-08-29 23:52:09 +0300

    create ansible files

---
+++ stage 2/ansible_build_inventory.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+# Parse ansible community.docker.docker_compose and build inventory file
+
+import sys
+import json
+data = sys.argv[1]
+
+data = json.loads(data)['services']
+hosts = {}
+
+for service in data:
+    hosts[service] = {}
+    for host in data[service]:
+        hosts[service][host] = {}
+        for network in data[service][host]['networks']:
+            ip = data[service][host]['networks'][network]['IPAddress']
+            try:
+                hosts[service][host]['ip'].append(ip)
+            except KeyError:
+                hosts[service][host]['ip'] = []
+                hosts[service][host]['ip'].append(ip)
+
+with open('inventory', 'w', encoding='utf8') as fh:
+    for service in hosts:
+        fh.write(f'[{service}]\n')
+        for host in hosts[service]:
+            for ip in hosts[service][host]['ip']:
+                fh.write(f'{ip}\n')
+        fh.write('\n')
